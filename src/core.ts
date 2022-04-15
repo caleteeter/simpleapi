64c8ca09b40c09dcb6d6fbb8cd2526916f86f356
Cale Teeter <cale.teeter@microsoft.com>
2022-04-15 16:11:26 -0400

    Initial commit

---
+++ src/core.ts
@@ -0,0 +1,16 @@
+import { createServer, Request, Response, Next } from 'restify';
+
+function respond(req: Request, res: Response, next: Next) {
+    res.send('hello ' + req.params.name);
+    next();
+}
+
+export function start() {
+    var server = createServer();
+    server.get('/hello/:name', respond);
+    server.head('/hello/:name', respond);
+
+    server.listen(8080, function() {
+        console.log('%s listening at %s', server.name, server.url);
+    });
+}
