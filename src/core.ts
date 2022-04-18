ce04c1e75bd24cf28892671755902e29672a48d2
Cale Teeter <cale.teeter@microsoft.com>
2022-04-18 17:10:30 -0400

    Test

--- src/core.ts
+++ src/core.ts
@@ -1,11 +1,10 @@
 import { createServer, Request, Response, Next } from 'restify';
-import cors from 'cors';
 
 function respond(req: Request, res: Response, next: Next) {
     if (req.params.name) {
         res.send('hello ' + req.params.name);
     } else {
-        res.send({"id": "hello there"});
+        res.send(JSON.stringify(req.headers));
     }
     next();
 }
@@ -16,12 +15,6 @@ export function start() {
     server.head('/hello', respond);
     server.get('/hello/:name', respond);
     server.head('/hello/:name', respond);
-
-    // server.use(cors({
-    //     origin: "*",
-    //     methods: 'GET,PUT,POST,DELETE',
-    //     allowedHeaders: ['Content-Type', 'api_key', 'Authorization']
-    // }));
 
     server.pre((req: Request, res: Response, next: Next)=> {
         res.header("Access-Control-Allow-Origin", "*");
