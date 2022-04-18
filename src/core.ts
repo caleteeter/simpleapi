483533e0744e5a1fe9a7ea02546b7acd00a5b51f
Cale Teeter <cale.teeter@microsoft.com>
2022-04-18 11:07:00 -0400

    test

--- src/core.ts
+++ src/core.ts
@@ -17,11 +17,18 @@ export function start() {
     server.get('/hello/:name', respond);
     server.head('/hello/:name', respond);
 
-    server.use(cors({
-        origin: "*",
-        methods: ['GET,PUT,POST,DELETE'],
-        allowedHeaders: ['Content-Type', 'api_key', 'Authorization']
-    }));
+    // server.use(cors({
+    //     origin: "*",
+    //     methods: 'GET,PUT,POST,DELETE',
+    //     allowedHeaders: ['Content-Type', 'api_key', 'Authorization']
+    // }));
+
+    server.pre((req: Request, res: Response, next: Next)=> {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        res.header("Access-Control-Allow-Headers", "Content-Type, api_key, Authorization")
+        next();
+    });
 
     server.listen(8080, function() {
         console.log('%s listening at %s', server.name, server.url);
