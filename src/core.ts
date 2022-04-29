639da90c53e84b36b4c0bd6e2d949da6ee89e379
Cale Teeter <cale.teeter@microsoft.com>
2022-04-29 10:23:45 -0400

    Update

--- src/core.ts
+++ src/core.ts
@@ -4,9 +4,14 @@ function respond(req: Request, res: Response, next: Next) {
     if (req.params.name) {
         res.send('hello ' + req.params.name);
     } else {
-        res.send(JSON.stringify(req.headers));
+        res.send('hello');
     }
     next();
+}
+
+function getDetailedData(req: Request, res: Response, next: Next) {
+    let testData = { 'name': 'get' };
+    res.send(testData);
 }
 
 export function start() {
@@ -15,15 +20,16 @@ export function start() {
     server.head('/hello', respond);
     server.get('/hello/:name', respond);
     server.head('/hello/:name', respond);
+    server.get('/details', getDetailedData);
 
-    server.pre((req: Request, res: Response, next: Next)=> {
+    server.pre((req: Request, res: Response, next: Next) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
         res.header("Access-Control-Allow-Headers", "Content-Type, api_key, Authorization")
         next();
     });
 
-    server.listen(8080, function() {
+    server.listen(8080, function () {
         console.log('%s listening at %s', server.name, server.url);
     });
 }
